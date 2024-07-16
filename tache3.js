f329480b774bb04f0334df6f1cd6a50956668bfe
Mouhamed Ndiaye <mouhamethndiaye123@gmail.com>
2024-07-16 11:42:40 +0000

    ajout des fichiers du tache3 de js

---
+++ tache3.js
@@ -0,0 +1,18 @@
+// Déclarez trois variables x, y et z comme "1", "2" et x+y. Afficher le type et la valeur de chaque variable. Ensuite, forcez le type de x et y à entier et exécutez à nouveau a+b. Afficher le type et la valeur de chaque variable.
+
+let x=1;
+let y=2;
+let z= x+y;
+
+document.write(typeof x)
+document.write(x)
+document.write(typeof y)
+document.write(y)
+document.write(typeof z)
+document.write(z)
+x=2.5
+y=3.4
+document.write(typeof x)
+document.write(x)
+document.write(typeof y)
+document.write(y)
